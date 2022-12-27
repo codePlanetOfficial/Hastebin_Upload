8e4a9a8fa352dea2e48c9ccd8fee9869cc5dbbb0
ZaimH <ZaimH@LAPTOP-ULU27CAT>
2022-12-27 12:31:04 +1030

    Add project files.

---
+++ Hastebin Upload/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Hastebin_Upload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.server_url = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.text_box_multiline = new System.Windows.Forms.TextBox();
+            this.upload_btn = new System.Windows.Forms.Button();
+            this.display_textbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // server_url
+            // 
+            this.server_url.Location = new System.Drawing.Point(98, 13);
+            this.server_url.Name = "server_url";
+            this.server_url.Size = new System.Drawing.Size(324, 22);
+            this.server_url.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server URL:";
+            // 
+            // text_box_multiline
+            // 
+            this.text_box_multiline.Location = new System.Drawing.Point(15, 67);
+            this.text_box_multiline.Multiline = true;
+            this.text_box_multiline.Name = "text_box_multiline";
+            this.text_box_multiline.Size = new System.Drawing.Size(407, 353);
+            this.text_box_multiline.TabIndex = 3;
+            // 
+            // upload_btn
+            // 
+            this.upload_btn.Location = new System.Drawing.Point(12, 426);
+            this.upload_btn.Name = "upload_btn";
+            this.upload_btn.Size = new System.Drawing.Size(410, 44);
+            this.upload_btn.TabIndex = 4;
+            this.upload_btn.Text = "Upload";
+            this.upload_btn.UseVisualStyleBackColor = true;
+            this.upload_btn.Click += new System.EventHandler(this.upload_btn_Click);
+            // 
+            // display_textbox
+            // 
+            this.display_textbox.Location = new System.Drawing.Point(12, 476);
+            this.display_textbox.Name = "display_textbox";
+            this.display_textbox.ReadOnly = true;
+            this.display_textbox.Size = new System.Drawing.Size(410, 22);
+            this.display_textbox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter Your Text Here:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkOrchid;
+            this.panel1.Location = new System.Drawing.Point(-32, 526);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(476, 78);
+            this.panel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 543);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.display_textbox);
+            this.Controls.Add(this.upload_btn);
+            this.Controls.Add(this.text_box_multiline);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.server_url);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Hastebin Uploader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox server_url;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox text_box_multiline;
+        private System.Windows.Forms.Button upload_btn;
+        private System.Windows.Forms.TextBox display_textbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Hastebin Upload/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using Newtonsoft.Json;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Net.Http;
+
+namespace Hastebin_Upload
+{
+    public partial class Form1 : Form
+    {
+        public void UploadToHastebin()
+        {
+            // Get the API endpoint URL from the server_url control
+            string apiUrl = "https://" + server_url.Text + "/documents";
+
+            // Get the text to be uploaded from the text_box_multiline control
+            string text = text_box_multiline.Text;
+
+            // Create a new WebClient instance
+            using (WebClient client = new WebClient())
+            {
+                // Set the content type to "application/json"
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+
+                // Convert the text to a byte array
+                byte[] data = Encoding.UTF8.GetBytes(text);
+
+                // Make the POST request to the API endpoint
+                byte[] response = client.UploadData(apiUrl, "POST", data);
+
+                // Deserialize the response JSON to a dictionary
+                Dictionary<string, string> responseDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(Encoding.UTF8.GetString(response));
+
+                // Display the URL of the created paste in the display_textbox control
+                display_textbox.Text = "https://" + server_url.Text + "/" + responseDict["key"];
+            }
+        }
+
+       
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void upload_btn_Click(object sender, EventArgs e)
+        {
+            UploadToHastebin();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
